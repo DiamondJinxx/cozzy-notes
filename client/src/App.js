ac4b08b43f1f99c2018501e759d8ba4b304f9b5c
Smotrov Dmitriy <jinxx_develop@mail.ru>
2023-03-09 02:13:43 +0300

    try call backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,15 @@ import Greetings from './Greetings';
 import Form from './Form';
 
 import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+
+
+const [currentTime, setCurrentTime] = useState(0);
+useEffect(() => {
+    fetch('/time').then(res => res).then(data =>{
+        setCurrentTime(data);
+    });
+}, []);
 
 const App = () => (
     <div className='App'>
@@ -11,8 +20,9 @@ const App = () => (
         <p>
           <Greetings firstName='Dashulik' lastName='Smotrova' />
           Not Edit <code>src/App.js</code> and save to reload. I like it
+          The current time is {currentTime}
         </p>
-        <Form/>
+
       </header>
     </div>
   );
--- server/server.py
+++ server/server.py
@@ -1,8 +1,14 @@
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.rout('/')
+@app.route('/')
 def hello():
     return 'nu privet, dorogoi'
+
+
+@app.route('/time')
+def current_time():
+    return f'{time.time()}'
